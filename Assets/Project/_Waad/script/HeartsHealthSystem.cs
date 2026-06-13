b03057366fd5339dd47f3a72172b86edea8ea146
Null <w3oodh40@gmail.com>
2026-06-13 16:44:38 +0300

    Damage

--- Assets/Project/_Waad/script/HeartsHealthSystem.cs
+++ Assets/Project/_Waad/script/HeartsHealthSystem.cs
@@ -1,6 +1,79 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class HeartsHealthSystem
 {
-    
+    public event EventHandler OnDamaged;
+
+    private List<Heart> heartList;
+
+    public HeartsHealthSystem(int heartAmount)
+    {
+        heartList = new List<Heart>();
+
+        for (int i = 0; i < heartAmount; i++)
+        {
+            Heart heart = new Heart(4);
+            heartList.Add(heart);
+        }
+    }
+
+    public List<Heart> GetHeartList()
+    {
+        return heartList;
+    }
+
+    public void Damage(int damageAmount)
+    {
+        for (int i = heartList.Count - 1; i >= 0; i--)
+        {
+            Heart heart = heartList[i];
+
+            if (damageAmount > heart.GetFragmentAmount())
+            {
+                damageAmount -= heart.GetFragmentAmount();
+                heart.Damage(heart.GetFragmentAmount());
+            }
+            else
+            {
+                heart.Damage(damageAmount);
+                break;
+            }
+        }
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public class Heart
+    {
+        private int fragments;
+
+        public Heart(int fragments)
+        {
+            this.fragments = fragments;
+        }
+
+        public int GetFragmentAmount()
+        {
+            return fragments;
+        }
+
+        public void SetFragments(int fragments)
+        {
+            this.fragments = fragments;
+        }
+
+        public void Damage(int damageAmount)
+        {
+            if (damageAmount >= fragments)
+            {
+                fragments = 0;
+            }
+            else
+            {
+                fragments -= damageAmount;
+            }
+        }
+    }
 }
