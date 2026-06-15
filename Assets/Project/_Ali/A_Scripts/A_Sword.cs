00c8a76e4b7474beb6cb7fe9bb87ae81a3309949
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-06-15 22:21:36 +0300

    save

---
+++ Assets/Project/_Ali/A_Scripts/A_Sword.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class A_Sword : MonoBehaviour
+{
+    
+
+
+
+    public void OnAttack(InputAction.CallbackContext context)
+    {
+
+    }
+}
