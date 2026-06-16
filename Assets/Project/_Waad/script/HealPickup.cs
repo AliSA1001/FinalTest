56e0aa3d44522087ed4c3e5bbee10ae0ec531e0c
w3oodh40-a11y <w3oodh40@gmail.com>
2026-06-16 10:50:49 +0300

    heal after kill enemy <3

--- Assets/Project/_Waad/script/HealPickup.cs
+++ Assets/Project/_Waad/script/HealPickup.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class HealPickup : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             HeartsHealthVisual heartsHealthVisual =
                 FindObjectOfType<HeartsHealthVisual>();
