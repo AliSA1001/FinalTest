8ca236dc0f8a86faaa704ea4a6fa151a0aa9a679
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-06-16 04:54:33 +0300

    save

--- Assets/Project/_Ali/A_Scripts/A_Sword.cs
+++ Assets/Project/_Ali/A_Scripts/A_Sword.cs
@@ -3,12 +3,43 @@ using UnityEngine.InputSystem;
 
 public class A_Sword : MonoBehaviour
 {
-    
 
+    [SerializeField] private Animator animator;
+    [SerializeField] private CharacterController characterController;
+
+
+    // call movement singleten
+    private A_Movement movementRef;
+
+
+
+    private bool _canAttack = true;
+
+
+    private void Start()
+    {
+        movementRef = A_Movement.instance;
+    }
+    private void HandleCantAttack()
+    {
+        _canAttack = true;
+
+        animator.SetLayerWeight(1, 0.43f);
+
+        movementRef.Canmove = true;
+
+    }
 
 
     public void OnAttack(InputAction.CallbackContext context)
     {
-
+        if (context.performed && _canAttack && characterController.isGrounded)
+        {
+            animator.SetTrigger("Attack1");
+            animator.SetLayerWeight(1, 0);
+            _canAttack = false;
+            movementRef.Canmove = false;
+            Invoke("HandleCantAttack", 1);
+        }
     }
 }
