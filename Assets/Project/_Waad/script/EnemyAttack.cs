1951162486dfc0df6e9402259071596a1e9a606b
w3oodh40-a11y <w3oodh40@gmail.com>
2026-06-17 19:30:33 +0300

    2

--- Assets/Project/_Waad/script/EnemyAttack.cs
+++ Assets/Project/_Waad/script/EnemyAttack.cs
@@ -4,12 +4,19 @@ public class EnemyAttack : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log("Something Hit: " + collision.gameObject.name);
+
         if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("Player Hit!");
+
             HeartsHealthVisual heartsHealthVisual =
                 FindObjectOfType<HeartsHealthVisual>();
 
-            heartsHealthVisual.Damage1();
+            if (heartsHealthVisual != null)
+            {
+                heartsHealthVisual.Damage1();
+            }
         }
     }
 }
