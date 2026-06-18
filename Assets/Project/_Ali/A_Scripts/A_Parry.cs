47400fa5880394560291f55640708a26f7800764
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-06-18 17:59:05 +0300

    Parry0.1

---
+++ Assets/Project/_Ali/A_Scripts/A_Parry.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class A_Parry : MonoBehaviour
+{
+    
+
+    public void OnParry(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+
+        }
+    }
+
+}
