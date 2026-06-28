5faef27fc7f5f361fd0ea08f575cfe2c3c3f81a4
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-06-28 18:38:55 +0300

    damage 2.0

--- Assets/Project/_Ali/A_Scripts/A_DamageHit.cs
+++ Assets/Project/_Ali/A_Scripts/A_DamageHit.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class A_DamageHit : MonoBehaviour
 {
+    [SerializeField] private float SwordDamage;
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.TryGetComponent<IDamageable>(out IDamageable damageable))
         {
-            damageable.TakeDamage(10);
+            damageable.TakeDamage(SwordDamage);
         }
     }
 }
