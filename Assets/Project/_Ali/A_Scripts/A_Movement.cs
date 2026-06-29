ec9c2d36d599b178de6e76eaff196326ab2a169d
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-06-30 00:16:14 +0300

    movement3.0

--- Assets/Project/_Ali/A_Scripts/A_Movement.cs
+++ Assets/Project/_Ali/A_Scripts/A_Movement.cs
@@ -85,6 +85,36 @@ public class A_Movement : MonoBehaviour
     }
     private void HandleLookingWhileCantMove()
     {
+        HandleLooking();
+    }
+    private void HandleJumpingCoolDown()
+    {
+
+        if (_isJumpCooldown)
+        {
+            jumpCooldown -= Time.deltaTime;
+            if (jumpCooldown < 0)
+            {
+                jumpCooldown = 1;
+                _isJumpCooldown = false;
+            }
+        }
+    }
+
+    private void Moving()
+    {
+        HandleLooking();
+
+        characterController.Move(moveDirection * Time.deltaTime * speed);
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+
+
+    }
+
+  private void HandleLooking()
+    {
         Vector3 cameraForward = Camera.main.transform.forward;
         Vector3 cameraRight = Camera.main.transform.right;
 
@@ -116,61 +146,6 @@ public class A_Movement : MonoBehaviour
 
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
         }
-    }
-    private void HandleJumpingCoolDown()
-    {
-
-        if (_isJumpCooldown)
-        {
-            jumpCooldown -= Time.deltaTime;
-            if (jumpCooldown < 0)
-            {
-                jumpCooldown = 1;
-                _isJumpCooldown = false;
-            }
-        }
-    }
-
-    private void Moving()
-    {
-        Vector3 cameraForward = Camera.main.transform.forward;
-        Vector3 cameraRight = Camera.main.transform.right;
-
-        cameraForward.y = 0f;
-        cameraRight.y = 0f;
-
-        cameraForward.Normalize();
-        cameraRight.Normalize();
-
-         moveDirection = (cameraForward * _zMovement) + (cameraRight * _xMovement);
-
-        // here we will check for the move direction if it is 0 then we dont do footstep effect
-        if((_zMovement > 0 || _xMovement > 0) && characterController.isGrounded)
-        {
-            footStep.SetActive(true);
-        }
-        else
-        {
-            footStep.SetActive(false);
-        }
-
-        if (characterController.isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        if(moveDirection.magnitude > 0.1f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
-
-            transform.rotation = Quaternion.RotateTowards(transform.rotation,targetRotation,rotationSpeed * Time.deltaTime);
-        }
-
-        characterController.Move(moveDirection * Time.deltaTime * speed);
-
-        velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
-
-
     }
 
     private void HandleAttackingMovement()
