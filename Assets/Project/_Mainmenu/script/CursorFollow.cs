341a3ef4aad48340700e83452a4283d503432e7e
Null <w3oodh40@gmail.com>
2026-07-05 01:13:08 +0300

    Main

--- Assets/Project/_Mainmenu/script/CursorFollow.cs
+++ Assets/Project/_Mainmenu/script/CursorFollow.cs
@@ -1,18 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class CursorFollow : MonoBehaviour
+public class CursorFollowUI : MonoBehaviour
 {
+    private RectTransform rect;
+    private Image image;
+
+    [Header("Cursor Sprites")]
+    public Sprite happyCursor;
+    public Sprite pointCursor;
+    public Sprite sadCursor;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+
     void Start()
     {
         Cursor.visible = false;
+        SetHappy();
     }
 
     void Update()
     {
-        Vector3 mouse = Input.mousePosition;
+        rect.position = Input.mousePosition;
+    }
 
-        mouse.z = 10f;
+    public void SetHappy()
+    {
+        image.sprite = happyCursor;
+    }
 
-        transform.position = Camera.main.ScreenToWorldPoint(mouse);
+    public void SetPoint()
+    {
+        image.sprite = pointCursor;
+    }
+
+    public void SetSad()
+    {
+        image.sprite = sadCursor;
+    }
+
+    void OnApplicationQuit()
+    {
+        Cursor.visible = true;
     }
 }
