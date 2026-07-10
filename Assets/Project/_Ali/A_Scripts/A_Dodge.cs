ccdf969a01c3e84c621220992901519dc3f43340
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-07-11 02:42:34 +0300

    Save for delating

--- Assets/Project/_Ali/A_Scripts/A_Dodge.cs
+++ Assets/Project/_Ali/A_Scripts/A_Dodge.cs
@@ -5,7 +5,12 @@ public class A_Dodge : MonoBehaviour
 {
 
     [SerializeField] private Animator animator;
+    [SerializeField] private Animator animatorAct1;
+    [SerializeField] private Animator animatorAct2;
+
     [SerializeField] private float speed;
+
+    
 
 
     private CharacterController _characterController;
@@ -14,15 +19,28 @@ public class A_Dodge : MonoBehaviour
     public bool isInvincbal = false;
 
 
+
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
 
         _movement = A_Movement.instance;
     }
+    private void Update()
+    {
+        if (!animatorAct1.gameObject.active)
+        {
+            animator = animatorAct2;
+        }
+        else if (!animatorAct2.gameObject.active)
+        {
+            animator = animatorAct1;
+        }
+    }
 
     private void SwordWeight()
     {
+        isInvincbal = false;
         _movement.CanMove = true;
         _movement.isDodging = false;
 
@@ -37,8 +55,9 @@ public class A_Dodge : MonoBehaviour
             animator.SetLayerWeight(1, 0);
             animator.SetTrigger("Dodge");
             _movement.isDodging = true;
-            
+            isInvincbal = true;
             Invoke("SwordWeight", 0.65f);
+            
 
         }
     }
