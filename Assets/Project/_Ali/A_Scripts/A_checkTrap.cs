a16acc484d87f385c98881b8a77e760937046dcf
Ali Al shaiban <146249627+AliSA1001@users.noreply.github.com>
2026-07-12 07:48:53 +0300

    save

--- Assets/Project/_Ali/A_Scripts/A_checkTrap.cs
+++ Assets/Project/_Ali/A_Scripts/A_checkTrap.cs
@@ -1,25 +1,42 @@
+using System;
+using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal; // Required for Exposure
 
 public class A_checkTrap : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private GameObject torchPickUp;
-
     [SerializeField] private GameObject lava;
-    [SerializeField] private Volume volume;
-    
+    [SerializeField] private AudioSource soundtrack;
 
     public bool isHoldingTorch = false;
+
+    private List<GameObject> lightObjects;
+    private List<GameObject> darkObjects;
+
     private void Update()
     {
         if (torchPickUp.transform.IsChildOf(player))
         {
             isHoldingTorch = true;
-            
+            RenderSettings.reflectionIntensity = 0; 
             lava.SetActive(false);
+            foreach (GameObject obj in lightObjects)
+            {
+                obj.SetActive(false);
+            }
+            soundtrack.pitch = 0.3f;
         }
-       
     }
+    private void Start()
+    {
+        GameObject[] foundLights = GameObject.FindGameObjectsWithTag("LightObject");
+        lightObjects = new List<GameObject>(foundLights);
+
+        GameObject[] foundDark = GameObject.FindGameObjectsWithTag("DarkObject");
+        darkObjects = new List<GameObject>(foundDark);
     }
+}
